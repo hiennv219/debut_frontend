55b5553a31a9b363f5bbcaa0874de3effb37a5ef
Thanh Tran <thanhtx@bitkoex.com>
2019-10-07 17:51:52 +0700

    style posts

--- resources/assets/js/home/pages/newsfeed/Index.vue
+++ resources/assets/js/home/pages/newsfeed/Index.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="">
-      <h2 class="text-center title-home-page">News Feed</h2>
-      <p class="text-center text-gray">{{ $t('index_page.welcome') }}</p>
+      <div class="posts">
+        <div class="post">
+          <h3 class="title">Bitcoin Price (BTC) Extending Decline Below Key Support</h3>
+          <span class="text-muted time">2019-10-07 17:30</span>
+          <p>In the past few days, there was a steady decline in bitcoin form the $8,500 resistence against the US Dollar.
+          The BTC/USD pair broke many support near $83,00 and $8,200 to enter a bearish zone.
+          Moreover, there was a close below the $8,200 level and the 100 hourly simple moving averge.
+          Recently, the bears gained traction and they were successful in pushing the price below the $8,000 support area.</p>
+        </div>
+        <div class="post">
+          <h3 class="title">Bitcoin Price (BTC) Extending Decline Below Key Support</h3>
+          <span class="text-muted time">2019-10-07 17:30</span>
+          <p>In the past few days, there was a steady decline in bitcoin form the $8,500 resistence against the US Dollar.
+          The BTC/USD pair broke many support near $83,00 and $8,200 to enter a bearish zone.
+          Moreover, there was a close below the $8,200 level and the 100 hourly simple moving averge.
+          Recently, the bears gained traction and they were successful in pushing the price below the $8,000 support area.</p>
+        </div>
+      </div>
 
   </div>
 </template>
@@ -23,8 +39,34 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .title-home-page{
   margin-top: 50px;
+}
+
+.posts {
+  padding: 50px;
+  .post{
+    background: #fff;
+    border-radius: 8px;
+    border: 1px solid #cdcdcd;
+    width: 90%;
+    margin-bottom: 50px;
+    padding: 1rem 10px;
+
+    .title{
+      font-size: 1.25rem;
+      margin-bottom: 0.7rem;
+    }
+    .time{
+      font-size: 0.9rem0000000000000;
+    }
+  }
+  .post:nth-child(even) {
+    float: right;
+  }
+  .post:nth-child(odd) {
+
+  }
 }
 </style>
